9fda7eda6c4daeae91301e24d11821141545128e
Satya Surya Benarji <mailtobenerjee@gmail.com>
2021-10-03 18:35:17 +0530

    sample sql file which has text insert statements

---
+++ text.sql
@@ -0,0 +1,164 @@
+insert into festivals values('2022-01-01','ఆంగ్ల సంవత్సరాదిి,మాస శివరాత్రి ');
+insert into festivals values('2022-01-02','వరల్డ్ నేచర్ డే , అమావాస్య ');
+insert into festivals values('2022-01-10','దుర్గాష్టమి వ్రతం');
+insert into festivals values('2022-01-12','నేషనల్ యూత్ డే ,స్వామి వివేకానంద జయంతి ');
+insert into festivals values('2022-01-13','పుష్య పుత్రాద ఏకాదశి,ముక్కోటి ఏకాదశి,భోగి');
+insert into festivals values('2022-01-14','పొంగల్ ,మకర సంక్రాంతి ,ఉత్తరాయణ పుణ్యకాలం ప్రారంభం');
+insert into festivals values('2022-01-15','శనిత్రయోదశి,ప్రదోష వ్రతం');
+insert into festivals values('2022-01-16','కనుము ,బొమ్మలనోము ,ముక్కనుము');
+insert into festivals values('2022-01-17','పౌర్ణమి');
+insert into festivals values('2022-01-23','నేతాజీ జయంతి');
+insert into festivals values('2022-01-24','శ్రావణ కార్తె');
+insert into festivals values('2022-01-25','భాను సప్తమి');
+insert into festivals values('2022-01-26','రిపబ్లిక్ డే');
+insert into festivals values('2022-01-28','షట్టిల ఏకాదశి');
+insert into festivals values('2022-01-30','మహాత్మాగాంధీ వర్ధంతి,ప్రదోష వ్రతం ,మాస శివరాత్రి');
+insert into festivals values('2022-01-31','అవతార్ మిహిర్ బాబా అమరతిథి ');
+
+
+insert into festivals values('2022-02-01','అమావాస్య,చొల్లంగి అమావాస్య');
+insert into festivals values('2022-02-03','శ్రీ మార్కండేయ మహర్షి జయంతి');
+insert into festivals values('2022-02-07','రధసప్తమి,సోమవారం వృతం');
+insert into festivals values('2022-02-08','భీష్మాష్టమి,దుర్గాష్టమి వ్రతం');
+insert into festivals values('2022-02-10','మధ్వ నవమి');
+insert into festivals values('2022-02-12','జయ ఏకాదశి');
+insert into festivals values('2022-02-13','కుంభ సంక్రమణం');
+insert into festivals values('2022-02-14','ప్రదోష వ్రతం,వాలెంటైన్స్ డే');
+insert into festivals values('2022-02-16','పౌర్ణమి,శ్రీ సత్యనారాయణ పూజ,సింధుస్నానం');
+insert into festivals values('2022-02-20','సంకటహర చతుర్థి');
+insert into festivals values('2022-02-26','స్వామి దయానంద సరస్వతి జయంతి');
+insert into festivals values('2022-02-27','ఏకాదశి');
+insert into festivals values('2022-02-28','సోమా ప్రదోష వ్రతం,నేషనల్ సైన్స్ డే ');
+
+
+insert into festivals values('2022-03-01','మహాశివరాత్రి,షబ్-ఎ-మేరాజ్');
+insert into festivals values('2022-03-02','అమావాస్య');
+insert into festivals values('2022-03-10','యాదాద్రి శ్రీలక్ష్మి నరసింహస్వామివారి బ్రహ్మౌత్సువాలు తిరుకళ్యాణం');
+insert into festivals values('2022-03-12','తిరుమల శ్రీవారి తెప్పోత్సవం ప్రారంభం');
+insert into festivals values('2022-03-13','ప్రదోష వ్రతం,మీన సంక్రమణం');
+insert into festivals values('2022-03-14','హోలిక దహన్,పౌర్ణమి వ్రతం,శ్రీ సత్యనారాయణ పూజ');
+insert into festivals values('2022-03-15','హోలీ పండుగ,పౌర్ణమి, ఉత్తరాభాద్ర కార్తె');
+insert into festivals values('2022-03-16','సంకటహర చతుర్థి');
+insert into festivals values('2022-03-17','పాపమోచనీ ఏకాదశి');
+insert into festivals values('2022-03-23','ప్రదోష వ్రతం');
+insert into festivals values('2022-03-24','రేవతి కార్తె');
+
+insert into festivals values('2022-04-01','ఏప్రిల్ ఫూల్ ,అమావాస్య,Bank Holiday');
+insert into festivals values('2022-04-02','ఉగాడి');
+insert into festivals values('2022-04-03','రంజాన్ నెల ప్రారంభం');
+insert into festivals values('2022-04-04','సోమవారం వృతం,మత్స్య జయంతి');
+insert into festivals values('2022-04-10','శ్రీరామ నవమి ,తాటాకు ఆదివారం');
+insert into festivals values('2022-04-11','ధర్మరాజు దశమి ');
+insert into festivals values('2022-04-12','కామద ఏకాదశి');
+insert into festivals values('2022-04-13','వైష్ణవ కామద ఏకాదశి');
+insert into festivals values('2022-04-14','మహావీర్ జయంతి ,అంబెడ్కర్ జయంతి ,ప్రదోష వ్రతం');
+insert into festivals values('2022-04-15','గుడ్ ఫ్రైడే');
+insert into festivals values('2022-04-16','హనుమజ్జయంతి,చైత్ర పూర్ణమి,పూర్ణమి');
+insert into festivals values('2022-04-19','సంకటహర చతుర్థి');
+insert into festivals values('2022-04-26','వరూధినీ ఏకాదశి');
+insert into festivals values('2022-04-28','షబ్-ఎ-ఖద్ర్ (లైలతుల్ ఖద్ర్) ,ప్రదోష వ్రతం');
+insert into festivals values('2022-04-30','అమావాస్య');
+
+
+insert into festivals values('2022-05-01','మే దే');
+insert into festivals values('2022-05-03','రంజాన్,అక్షయ తృతీయ');
+insert into festivals values('2022-05-12','మోహిని ఏకాదశి');
+insert into festivals values('2022-05-13','పరశురామ ద్వాదశి,ప్రదోష వ్రతం');
+insert into festivals values('2022-05-16','పౌర్ణమి ,వైశాఖి పూర్ణిమ ,బుద్ధ పూర్ణిమ');
+insert into festivals values('2022-05-19','ఏకదంట సంకష్టహర చతుర్థి');
+insert into festivals values('2022-05-25','రోహిణి కార్తె ');
+insert into festivals values('2022-05-26','అపార ఏకాదశి ');
+insert into festivals values('2022-05-27','ప్రదోష్ వ్రతం');
+insert into festivals values('2022-05-30','అమావాస్య ');
+
+
+insert into festivals values('2022-06-02','తెలంగాణా రాష్ట్ర అవతరణ దినోత్సవము ');
+insert into festivals values('2022-06-10','నిర్జల ఏకాదశి ,దశాపాపహర దశమి ,');
+insert into festivals values('2022-06-11','తదనంతర నిర్జల ఏకాదశీ,వైష్ణవ నిర్జల ఏకాదశీ,రామలక్ష్మణ ద్వాదశి');
+insert into festivals values('2022-06-12','ప్రదోష్ వ్రతం');
+insert into festivals values('2022-06-14','పౌర్ణమి ,ఏరువాక పౌర్ణమి');
+insert into festivals values('2022-06-17','సంకటహర చతుర్థి ,కృష్ణపింగళా సంకష్టహర చతుర్థి');
+insert into festivals values('2022-06-24','యోగిని ఏకాదశీ');
+insert into festivals values('2022-06-26','ప్రదోష్ వ్రతం');
+insert into festivals values('2022-06-29','అమావాస్య ');
+
+
+insert into festivals values('2022-07-04','అల్లూరి సీతారామ రాజు జయంతి ,');
+"insert into festivals values('2022-07-10','బక్రీద్ ,శయన ఏకాదశి,దేవశయనీ ఏకాదశీ,వాసుదేవ ద్వాదశి,
+బోనాలు ');"
+insert into festivals values('2022-07-11','ప్రదోష్ వ్రతం');
+insert into festivals values('2022-07-13','పౌర్ణమి ,గురు పూర్ణిమ ');
+insert into festivals values('2022-07-16','గజానన సంకష్టహర చతుర్థి');
+insert into festivals values('2022-07-17','దక్షిణాయనం ప్రారంభం,బోనాలు ');
+insert into festivals values('2022-07-24','కామిక ఏకాదశీ');
+insert into festivals values('2022-07-25','ప్రదోష్ వ్రతం');
+insert into festivals values('2022-07-28','అమావాస్య ');
+
+
+insert into festivals values('2022-08-02','నాగ పంచమి ');
+insert into festivals values('2022-08-08','ఆషూరా దినం (మొహర్రం) ,శ్రావణ పుత్రద ఏకాదశి');
+insert into festivals values('2022-08-09','ప్రదోష వ్రతం,దామోదర ద్వాదశి');
+insert into festivals values('2022-08-11','రాఖీ ,రక్షా బంధన్,యజుర్వేద ఉపాకర్మ');
+insert into festivals values('2022-08-12','వరలక్ష్మి వ్రతం,పౌర్ణమి,శ్రావణ పూర్ణిమ ');
+insert into festivals values('2022-08-15','భారత స్వాతంత్య్ర దినోత్సవం,సంకటహర చతుర్థి');
+insert into festivals values('2022-08-18','శ్రీకృష్ణాష్టమి,మఖ కార్తె ');
+insert into festivals values('2022-08-19','జన్మాష్టమి *ఇస్కాన్');
+insert into festivals values('2022-08-23','అజ ఏకాదశీ');
+insert into festivals values('2022-08-24','ప్రదోష వ్రతం ');
+insert into festivals values('2022-08-27','అమావాస్య ');
+insert into festivals values('2022-08-28','ముహర్రం ముగుస్తుంది');
+insert into festivals values('2022-08-31','వినాయక చవితి ,గణేష్ చతుర్థి');
+
+
+insert into festivals values('2022-09-05','గురు పూజోత్సవం');
+insert into festivals values('2022-09-06','పార్శ్వ ఏకాదశి,పరివర్తినీ ఏకాదశీ');
+insert into festivals values('2022-09-07','వైష్ణవ పరివర్తినీ ఏకాదశీ,వామన జయంతి');
+insert into festivals values('2022-09-08','ప్రదోష వ్రతం,ఓనం');
+insert into festivals values('2022-09-09','గణేష్ నిమజ్జనం');
+insert into festivals values('2022-09-10','పౌర్ణమి');
+insert into festivals values('2022-09-13','విఘ్నరాజ్ సంకష్టహర చతుర్థి,ఉత్తర కార్తె,సంకటహర చతుర్థి');
+insert into festivals values('2022-09-21','ఇందిర ఏకాదశి');
+insert into festivals values('2022-09-23','ప్రదోష్ వ్రతం');
+insert into festivals values('2022-09-25','అమావాస్య,బతుకమ్మ ప్రారంభం');
+insert into festivals values('2022-09-26','దేవి శరన్నవరాత్రి ప్రారంభం');
+
+
+insert into festivals values('2022-10-02','గాంధీ జయంతి ,లాల్ బహదూర్ శాస్త్రి జయంతి,దుర్గ పూజ');
+insert into festivals values('2022-10-03','దుర్గాష్టమి ');
+insert into festivals values('2022-10-04','విజయ దశమి');
+insert into festivals values('2022-10-05','దశహర');
+insert into festivals values('2022-10-06','పాశాంకుశ ఏకాదశీ,పద్మనాభ ద్వాదశి');
+insert into festivals values('2022-10-07','ప్రదోష వ్రతం ');
+insert into festivals values('2022-10-09','పౌర్ణమి ,వాల్మీకి జయంతి ');
+insert into festivals values('2022-10-10','చిత్త కార్తె');
+insert into festivals values('2022-10-12','అట్లతద్ది,ఉండ్రాళ్ళ తద్దె ');
+insert into festivals values('2022-10-13','వక్రతుండా సంకష్టహర చతుర్థి');
+insert into festivals values('2022-10-21','రమ ఏకాదశి ');
+insert into festivals values('2022-10-22','శని త్రయోదశి,ప్రదోష్ వ్రతం');
+insert into festivals values('2022-10-24','లక్ష్మి పూజ,దీపావళి,నరక చతుర్ధశి');
+insert into festivals values('2022-10-25','అమావాస్య ');
+insert into festivals values('2022-10-28','నాగుల చవితి ');
+
+
+insert into festivals values('2022-11-04','దేవుత్తన ఏకాదశీ,ప్రబోధిని ఏకాదశి, కార్తీక శుద్ధ ఏకాదశి');
+insert into festivals values('2022-11-05','శనిత్రయోదశి,ప్రదోష వ్రతం,యోగేశ్వర ద్వాదశి');
+insert into festivals values('2022-11-08','కార్తీక పౌర్ణమి,పౌర్ణమి ,గురునానక్ జయంతి');
+insert into festivals values('2022-11-12','గణాధిప సంకష్టహర చతుర్థి');
+insert into festivals values('2022-11-14','బాలల దినోత్సవం,జవహర్ లాల్ నెహ్రూ జయంతి');
+insert into festivals values('2022-11-20','ఉత్పన్న ఏకాదశీ');
+insert into festivals values('2022-11-21','ప్రదోష్ వ్రతం');
+insert into festivals values('2022-11-23','అమావాస్య ');
+insert into festivals values('2022-11-28','సుబ్రహ్మణ్య షష్ఠి,నాగ పంచమి *తెలుగు');
+
+
+insert into festivals values('2022-12-01','ఎయిడ్స్ డే');
+insert into festivals values('2022-12-03','మోక్షద ఏకాదశి,జ్యేష్ఠ కార్తె');
+insert into festivals values('2022-12-04','వైష్ణవ మోక్షద ఏకాదశి,మత్స్య ద్వాదశి');
+insert into festivals values('2022-12-05','ప్రదోష వ్రతం ');
+insert into festivals values('2022-12-08','పౌర్ణమి');
+insert into festivals values('2022-12-11','ఆఖురత సంకష్టహర చతుర్థి,సంకటహర చతుర్థి ');
+insert into festivals values('2022-12-19','సఫల ఏకాదశి ');
+insert into festivals values('2022-12-21','ప్రదోష వ్రతం');
+insert into festivals values('2022-12-23','అమావాస్య ');
+insert into festivals values('2022-12-24','క్రిస్టమస్ ఈవ్');
+insert into festivals values('2022-12-25','క్రిస్టమస్');
